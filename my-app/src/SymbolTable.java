112d3002f9425098cabd886c8edbc39363c4e3be
FrederikSkipper <RK23KN@student.aau.dk>
2026-04-22 00:27:08 +0200

    feat: A bit more set up

--- my-app/src/SymbolTable.java
+++ my-app/src/SymbolTable.java
@@ -1,4 +1,12 @@
 /* SEE PAGE 38, 39 IN CocoR Taste example */
+public class symbol { // object describing a declared symbol in symboltable
+    public string name; // name of symbol
+    public int type; // 
+    public int kind; // The kind of symbol variable, function, component
+    /* MISSING MORE */
+
+}
+
 public class SymbolTable {
     // types
     
