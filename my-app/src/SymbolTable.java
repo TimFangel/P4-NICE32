032bc6016f417c9513b0c7b580d01cc4955f8eb2
FrederikSkipper <RK23KN@student.aau.dk>
2026-04-22 15:35:49 +0200

    feat: Scope thingies

--- my-app/src/SymbolTable.java
+++ my-app/src/SymbolTable.java
@@ -1,8 +1,11 @@
 /* SEE PAGE 38, 39 IN CocoR Taste example */
 
 // object describing a declared symbol in symboltable, used for lookup of identifiers.
-@RequiredArgsConstructor 
-public class symbol { 
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Symbol { 
     public string name; // name of symbol
     public int type; // type of symbol
     public int kind; // The kind of symbol: variable, function, component, constant
@@ -20,7 +23,23 @@ public class SymbolTable {
     let component = 2;
     let scope = 3;
 
+    public Object topScope;
+    public int currentScopelevel;
+
     Parser parser;
+
+    public void OpenNewScope() {
+        Symbol scopeObj = new Symbol();
+        scope.name = "";
+        scope.next = topScope;
+        topScope = scopeObj;
+        currentScopelevel++;
+    }
+
+    public void closeCurrentScope() {
+        topScope =
+        currentScopelevel--;
+    }
 
     public SymbolTable (Parser parser) {
     this.parser = parser;
