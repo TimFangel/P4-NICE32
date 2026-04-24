a3755d3a51d2e2b5f1b227347b2f725c1a1e2f0a
Tim <fangeltim@gmail.com>
2026-04-24 10:51:57 +0200

    feat: initial function idea, might change depending on program.

--- my-app/src/main/java/com/mycompany/app/ast/statement/CompStmt.java
+++ my-app/src/main/java/com/mycompany/app/ast/statement/CompStmt.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 
 /* Composition */
 @Getter
-public class Comp extends Stmt { 
+public class CompStmt extends Stmt { 
     private Stmt statement1;
     private Stmt statement2;
 
-    Comp(int lineNumber, Stmt statement1, Stmt statement2) {
+    CompStmt(int lineNumber, Stmt statement1, Stmt statement2) {
         super(lineNumber);
         this.statement1 = statement1;
         this.statement2 = statement2;
