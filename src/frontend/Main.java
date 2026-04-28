2bd466bfbd74323c3b1a41e2dd7013e2bdaa4897
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2026-04-29 01:21:04 +0200

    fix: more imports

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -8,7 +8,7 @@ import frontend.semantic_analysis.TypeChecker;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Usage: java src.Main <file>");
+            System.out.println("Usage: java frontend.Main <file>");
             return;
         }
 
--- src/frontend/symboltable/Symbol.java
+++ src/frontend/symboltable/Symbol.java
@@ -12,7 +12,7 @@ public class Symbol {
     public String name; // Name of symbol
     public int type; // Type of symbol: int, float, bool
     public Symbol next; // Next symbol in same scope
-    public int category; // The category of symbol: variable, constant, function, component, constant, scope
+    public int category; // The category of symbol: variable, constant, function, component, scope
     public Symbol locals; // The local symbols 
     public int level; // 0 = global, 1 = local
 }
