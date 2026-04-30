9e4882055ee3a9c632a219b3f8028ee45965edde
Svolle <svolle777@icloud.com>
2026-04-30 12:40:55 +0200

    feat: added int and float records

---
+++ src/frontend/abstract_syntax/value/FloatNum.java
@@ -0,0 +1,5 @@
+package frontend.abstract_syntax.value;
+
+/* Decimal value */
+public record FloatNum(float value) implements Value {
+}
---
+++ src/frontend/abstract_syntax/value/IntNum.java
@@ -0,0 +1,5 @@
+package frontend.abstract_syntax.value;
+
+/* Numeral Value */
+public record IntNum(int value) implements Value {
+}
