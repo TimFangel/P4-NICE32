de232f1d5aa5aca5e4bd828a42172e89e8cdbbab
Svolle <svolle777@icloud.com>
2026-04-30 09:58:57 +0200

    refactor: renamed folder

---
+++ tests/frontend/symboltabletest/SymbolTableTest.java
@@ -0,0 +1,25 @@
+package frontend.symboltabletest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import frontend.coco.Parser;
+import frontend.symboltable.Symbol;
+import frontend.symboltable.SymbolTable;
+
+// Ongoing
+public class SymbolTableTest {
+    @Test
+    public void testNewSymbol() {
+        // Symbol symbol = new Symbol();
+        // symbol.setName("x");
+        // symbol.setCategory(1);
+        // symbol.setType(3);
+        // SymbolTable symboltable = new SymbolTable(Parser parser);
+        // Symbol result = symboltable.newSymbol("x", 1, 3);
+        // Assertions.assertEquals(symbol.getName(), result.getName());
+        // Assertions.assertEquals(symbol.getCategory(), result.getCategory());
+        // Assertions.assertEquals(symbol.getType(), result.getType());
+        Assertions.assertTrue(true);
+    }
+}
