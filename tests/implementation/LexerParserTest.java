f9d5f20755f15035fa8cf1fdc9be980f27963544
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2026-05-04 01:07:30 +0200

    feat: added prettify, file reader and comparison

--- tests/implementation/LexerParserTest.java
+++ tests/implementation/LexerParserTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
@@ -14,10 +16,10 @@ import frontend.coco.Parser;
 import frontend.coco.Scanner;
 
 class LexerParserTest {
-    final String basePath = "./tests/resources/implementation/lexer-parser/";
-    final String codePath = "code/";
+    final String basePath = ".\\tests\\resources\\implementation\\lexer-parser\\";
+    final String codePath = "code\\";
     final String codeFileExtension = ".NICE";
-    final String astPath = "ast/";
+    final String astPath = "ast\\";
     final String astFileExtension = ".txt";
 
     @Test
@@ -25,20 +27,33 @@ class LexerParserTest {
         // Read all filenames in the directories
         List<String> codeFiles = listFilesForFolder(new File(basePath + codePath), codeFileExtension);
         List<String> astFiles = listFilesForFolder(new File(basePath + astPath), astFileExtension);
-
+        
         // Run test for each code file
         for (String codeFile : codeFiles) {
             String astFile = codeFile.replace(basePath + codePath, basePath + astPath).replace(codeFileExtension,
-                    astFileExtension);
+                astFileExtension);
             // Check that ast file matching code file exists
             if (!astFiles.contains(astFile)) {
                 fail("Could not find ast file: " + astFile + ".");
                 continue;
             }
 
-            String ast = assertDoesNotThrow(() -> parse(codeFile));
+            // Read expected output
+            String ast = "";
+            try (java.util.Scanner astScanner = new java.util.Scanner(new File(astFile))) {
+                ast = removeWhiteSpaces(astScanner.useDelimiter("\\Z").next());
+            } catch (FileNotFoundException e) {
+                fail("Could not read ast: " + astFile);
+            } catch (NoSuchElementException e) {
+                fail("ast file was empty");
+            }
 
-            System.out.println(ast);
+            // Parse input
+            String parsedAst = removeWhiteSpaces(assertDoesNotThrow(() -> parse(codeFile)));
+
+            if (ast.compareTo(parsedAst) != 0) {
+                fail(String.format("AST' does not match. Expected:%n'''%n%s%n'''%nAnd got:%n'''%n%s%n'''", prettifyAst(ast), prettifyAst(parsedAst)));
+            }
         }
 
     }
@@ -50,9 +65,10 @@ class LexerParserTest {
     public List<String> listFilesForFolder(final File folder, String fileExtension) {
         List<String> list = new ArrayList<>();
 
+        // Read every file in directory
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
+                list.addAll(listFilesForFolder(fileEntry, fileExtension));
             } else if (!fileEntry.getPath().endsWith(fileExtension)) {
                 fail("Expected file with extension '" + fileExtension + "' but got '" + fileEntry.getPath() + "'.");
             } else {
@@ -63,6 +79,7 @@ class LexerParserTest {
         return list;
     }
 
+    // Main program copy
     String parse(String fileName) {
         Program ast = null;
         try {
@@ -84,5 +101,42 @@ class LexerParserTest {
         }
 
         return ast.toString();
+    }
+
+    String prettifyAst(String in) {
+        if (in == null) return "";
+
+        int indentSize = 2;
+        int indentLevel = 0;
+        StringBuilder out = new StringBuilder();
+
+        // add new lines around grouping tokens
+        in = in.replaceAll("([\\(\\[\\{,])", "$1\n");
+        in = in.replaceAll("([\\)\\]\\}])", "\n$1");
+
+        // add indents
+        for (String line : in.split("\\n")) {
+            line = line.strip();
+
+            if (line.isEmpty()) continue;
+
+            if (line.startsWith(")") || line.startsWith("]") || line.startsWith("}")) {
+                indentLevel = Math.max(0, indentLevel - 1);
+            }
+
+            int spaces = Math.max(0, indentLevel * indentSize);
+            if (spaces > 0) out.append(" ".repeat(spaces));
+            out.append(line).append('\n');
+
+            if (line.endsWith("(") || line.endsWith("[") || line.endsWith("{")) {
+                indentLevel++;
+            }
+        }
+
+        return out.toString().trim();
+    }
+
+    String removeWhiteSpaces(String in) {
+        return in.replaceAll("\\s+", "");
     }
 }
