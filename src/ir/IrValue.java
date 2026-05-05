38f1733be42ca2f0ded6ff36a9db5408aabae4a3
Tim <fangeltim@gmail.com>
2026-05-05 11:41:03 +0200

    docs: comment and indenting.

--- src/ir/IrValue.java
+++ src/ir/IrValue.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class IrValue {
-    private String name;    // t1
+    private String name;    // t1, 2, 2.5, true
     private Type type;       // type from symbol table. -1 = label, -2 = goto
 
     public IrValue(String name, Type type) {
